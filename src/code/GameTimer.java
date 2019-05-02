9c28103db8406fd748150ead2d1e4d3bdb718d4b
Wisa Powthongchin <wisa.pow@gmail.com>
2019-05-02 21:49:04 +0700

    created GameTimer

---
+++ src/code/GameTimer.java
@@ -0,0 +1,27 @@
+package code;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameTimer {
+
+    // Default time
+    private int time = 60;
+
+    public void countDown(Runnable tick) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                time--;
+                tick.run();
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 1000, 1000);
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
