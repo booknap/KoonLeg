64654848a5f4aa7a7d9fb1bd8c6ee7bdfc9ab9e0
booknap <napasai45@gmail.com>
2019-05-02 15:29:13 +0700

    fixed

--- src/sample/ConsoleTest.java
+++ src/sample/ConsoleTest.java
@@ -6,51 +6,51 @@ import java.util.TimerTask;
 
 public class ConsoleTest {
 
-    int secondsPassed = 0;
-    Timer myTimer = new Timer();
-    TimerTask task = new TimerTask() {
-        @Override
-        public void run() {
-            secondsPassed++;
-            System.out.println(secondsPassed);
-        }
-    };
-
-    public void start() {
-        myTimer.scheduleAtFixedRate(task, 1000, 1000);
-    }
+//    int secondsPassed = 0;
+//    Timer myTimer = new Timer();
+//    TimerTask task = new TimerTask() {
+//        @Override
+//        public void run() {
+//            secondsPassed++;
+//            System.out.println(secondsPassed);
+//        }
+//    };
+//
+//    public void start() {
+//        myTimer.scheduleAtFixedRate(task, 1000, 1000);
+//    }
 
     public static void main(String[] args) {
 
-        ConsoleTest c = new ConsoleTest();
-        c.start();
-
-//        Scanner sc = new Scanner(System.in);
-////        int input = 5;
-////        int multiplier = 12;
-////        Question q = new Question(input,multiplier);
-////        System.out.printf("%d x %d\n", input, multiplier);
-////        q.setChoice();
-////        for(int i = 0; i < 4; i++) {
-////            System.out.printf("%d) %d\n", i+1, q.getChoice(i));
-////        }
-////        if(q.getAnswer() == sc.nextInt()) {
-////            System.out.println("Correct");
-////        } else {
-////            System.out.println("Wrong");
-////        }
+//        ConsoleTest c = new ConsoleTest();
+//        c.start();
 //
-//        QuestionBuilder q = QuestionBuilder.getInstance();
-//        for (int i = 0; i < 10; i++) {
-//            Question question = q.bulidQuestion(4);
-//            System.out.printf("%d) %d X %d\n", i + 1, question.getX(), question.getY());
-//            question.setChoice();
-//            for (int j = 0; j < 4; j++) {
-//                System.out.printf(" %d. %d\n", j + 1, question.getChoice(j));
-//            }
-//            if (question.getAnswer() == sc.nextInt()) System.out.println("Correct\n");
-//            else System.out.println("Wrong\n");
+        Scanner sc = new Scanner(System.in);
+//        int input = 5;
+//        int multiplier = 12;
+//        Question q = new Question(input,multiplier);
+//        System.out.printf("%d x %d\n", input, multiplier);
+//        q.setChoice();
+//        for(int i = 0; i < 4; i++) {
+//            System.out.printf("%d) %d\n", i+1, q.getChoice(i));
 //        }
+//        if(q.getAnswer() == sc.nextInt()) {
+//            System.out.println("Correct");
+//        } else {
+//            System.out.println("Wrong");
+//        }
+
+        QuestionBuilder q = QuestionBuilder.getInstance();
+        for (int i = 0; i < 10; i++) {
+            Question question = q.bulidQuestion(4);
+            System.out.printf("%d) %d X %d\n", i + 1, question.getX(), question.getY());
+            question.setChoice();
+            for (int j = 0; j < 4; j++) {
+                System.out.printf(" %d. %d\n", j + 1, question.getChoice(j));
+            }
+            if (question.getAnswer() == sc.nextInt()) System.out.println("Correct\n");
+            else System.out.println("Wrong\n");
+        }
 
     }
 
